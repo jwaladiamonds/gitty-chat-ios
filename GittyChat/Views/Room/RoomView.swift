c7576725ef11671c28f6a07fed9d0018d8247d2a
Nikhil John <nikhiljohn1010@gmail.com>
2020-12-28 16:31:34 +0530

    Fixed keyboard action

--- GittyChat/Views/Room/RoomView.swift
+++ GittyChat/Views/Room/RoomView.swift
@@ -12,6 +12,9 @@ struct RoomView: View {
     @EnvironmentObject var gitter: Gitter
     var body: some View {
         ChatView(room: room)
+            .onTapGesture {
+                UIApplication.shared.windows.first?.rootViewController?.view.endEditing(true)
+            }
             .onAppear{
                 gitter.selectedRoom = room
             }
