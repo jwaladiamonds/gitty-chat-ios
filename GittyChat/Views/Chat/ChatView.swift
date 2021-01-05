02f65d47d19c1f417b437520a53cd99a62347e00
Nikhil John <nikhiljohn1010@gmail.com>
2021-01-05 15:59:16 +0530

    Added old view

--- GittyChat/Views/Chat/ChatView.swift
+++ GittyChat/Views/Chat/ChatView.swift
@@ -26,3 +26,36 @@ struct ChatView: View {
         .navigationBarTitle(room.name, displayMode: .inline)
     }
 }
+
+//struct ChatViewOld: View {
+//    var room: GRoom
+//    @State private var text = ""
+//    @State private var messageCount: Int = 60
+//    @EnvironmentObject var gitter: Gitter
+//    var body: some View {
+//        ScrollViewReader { scrollView in
+//            VStack(spacing: 0) {
+//                ScrollView(.vertical, showsIndicators: false) {
+//                    LazyVStack(alignment: .leading) {
+//                        ForEach(0 ..< messageCount) { item in
+//                            Text("Hello \(item)").id(item)
+//                        }
+//                    }
+//                    .padding(.horizontal)
+//                    .frame(maxWidth: .infinity, alignment: .leading)
+//                    .onAppear{
+//                        scrollView.scrollTo(messageCount-1, anchor: .bottom)
+//                    }
+//                }
+//
+//                MessageField(text: $text) {
+//                    print("Sending following text:")
+//                    print(self.text)
+//                    withAnimation {
+//                        scrollView.scrollTo(messageCount-1, anchor: .bottom)
+//                    }
+//                }
+//            }
+//        }
+//    }
+//}
